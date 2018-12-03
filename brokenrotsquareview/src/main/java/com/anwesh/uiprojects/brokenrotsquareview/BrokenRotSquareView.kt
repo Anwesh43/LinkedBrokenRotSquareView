314e6096d192f4a36c3c01d9ba87ac20f9936150
Anwesh43 <anweshthecool0@gmail.com>
2018-12-04 00:51:53 +0530

    adding BRSNode class

--- brokenrotsquareview/src/main/java/com/anwesh/uiprojects/brokenrotsquareview/BrokenRotSquareView.kt
+++ brokenrotsquareview/src/main/java/com/anwesh/uiprojects/brokenrotsquareview/BrokenRotSquareView.kt
@@ -116,4 +116,48 @@ class BrokenRotSquareView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BRSNode(var i : Int, val state : State = State()) {
+
+        private var next : BRSNode? = null
+
+        private var prev : BRSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BRSNode(i + 1)
+                next?.prev = this
+            }
+        }
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBRSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BRSNode {
+            var curr : BRSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
