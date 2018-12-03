32a57d8985ac920cc7fdf31e1a42d2de8e5e0787
Anwesh43 <anweshthecool0@gmail.com>
2018-12-04 01:34:41 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbrokenrotsquareview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbrokenrotsquareview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbrokenrotsquareview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.brokenrotsquareview.BrokenRotSquareView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BrokenRotSquareView.create(this)
     }
 }
