68998bfb1e35885d9829cd80c756df1bd7d538f3
Anwesh43 <anweshthecool0@gmail.com>
2018-12-03 23:49:14 +0530

    adding drawBRSNode canvas extensoon method

--- brokenrotsquareview/src/main/java/com/anwesh/uiprojects/brokenrotsquareview/BrokenRotSquareView.kt
+++ brokenrotsquareview/src/main/java/com/anwesh/uiprojects/brokenrotsquareview/BrokenRotSquareView.kt
@@ -17,7 +17,6 @@ val nodes : Int = 5
 val bars : Int = 2
 val color : Int = Color.parseColor("#4CAF50")
 val sizeFactor : Int = 3
-val strokeFactor : Int = 90
 val scDiv : Double = 0.51
 val scGap : Float = 0.05f
 
@@ -31,3 +30,24 @@ fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.getInv
 
 fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
 
+fun Canvas.drawBRSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = color
+    val size : Float = gap / sizeFactor
+    save()
+    translate(gap * (i + 1), h/2)
+    rotate(90f * sc2)
+    for (j in 0..(bars - 1)) {
+        val sc : Float = sc1.divideScale(j, bars)
+        save()
+        scale(1f - 2 * j, 1f)
+        rotate(90f * sc)
+        drawRect(RectF(-size/2, -size - size/4, 0f, -size/4), paint)
+        restore()
+    }
+    restore()
+}
