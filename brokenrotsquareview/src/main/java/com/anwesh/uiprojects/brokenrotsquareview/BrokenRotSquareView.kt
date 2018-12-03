b3fcea209417050bbd7d35b35b2b5f96565bc58a
Anwesh43 <anweshthecool0@gmail.com>
2018-12-03 23:22:34 +0530

    adding libraries and defining constants

--- brokenrotsquareview/src/main/java/com/anwesh/uiprojects/brokenrotsquareview/BrokenRotSquareView.kt
+++ brokenrotsquareview/src/main/java/com/anwesh/uiprojects/brokenrotsquareview/BrokenRotSquareView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.brokenrotsquareview
 /**
  * Created by anweshmishra on 03/12/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val bars : Int = 2
+val color : Int = Color.parseColor("#4CAF50")
+val sizeFactor : Int = 3
+val strokeFactor : Int = 90
+val scDiv : Double = 0.51
+val scGap : Float = 0.05f
